a0d3f6ea0476fdfc63972862fda81ea2dcafe3b1
fdawg <jaquanmkelsor@gmail.com>
2014-10-26 20:56:03 -0400

    Update db.sql

--- db.sql
+++ db.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `fdawg_rudrag_intern`
+-- Database: `rudrag_intern`
 --
 
 -- --------------------------------------------------------
